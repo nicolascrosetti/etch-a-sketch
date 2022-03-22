8e2eceefc42a41f1590cd13e50aa2a3b749df598
Nico <nccrosetti@gmail.com>
2022-03-22 09:48:11 -0300

    Create index.html, script.js and style.css

---
+++ script.js
@@ -0,0 +1,45 @@
+function createSquares(number){
+    const totalSquaresNumber = number * number;
+    const squareSize = 1250 / totalSquaresNumber;
+
+    //Changes grid-template-column property according to the grid size
+    let autoAmount = '';
+    for(i=0;i<number;i++){
+        autoAmount += 'auto ';
+    }
+    container.style.gridTemplateColumns = autoAmount;
+
+    //Adds Squares to container
+    for(i=0;i<totalSquaresNumber;i++){
+        
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.padding = squareSize+"px";
+        container.appendChild(square); 
+
+        //Changes background-color when a square is hovered
+        square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = 'black';
+        });
+    }
+}
+
+const container = document.querySelector(".container");
+const btnChange = document.querySelector("#btn-change");
+
+createSquares(16);
+
+btnChange.addEventListener('click', function(){
+    let newSize = 0;
+
+    container.innerHTML = '';
+    newSize = window.prompt('Enter amount of squares: (max amount: 100)');
+    while(newSize > 100){
+        newSize = window.prompt('Enter amount of squares: (max amount: 100)');
+    }
+    createSquares(newSize);
+});
+
+
+
+
